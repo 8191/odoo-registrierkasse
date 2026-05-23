3f19d6189b8b6084d4c8d3e1336ec0029f430d80
Manuel Faux <manuel@conf.at>
2026-05-23 16:43:21 +0200

    Add action to sign multiple orders at once

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         ],
     },
     'data': [
+        'data/pos_order_data.xml',
         'views/pos_config.xml',
         'views/point_of_sale_dashboard.xml',
         'views/res_config_settings_view.xml',
