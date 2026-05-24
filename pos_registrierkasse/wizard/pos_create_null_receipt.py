c0e63b54634f90af7fa7870ac0d6123c58728609
Manuel Faux <manuel@conf.at>
2026-05-24 11:11:41 +0200

    Add function to dashboard to create null receipt

---
+++ pos_registrierkasse/wizard/pos_create_null_receipt.py
@@ -0,0 +1,31 @@
+from odoo import fields, models, _
+from odoo.api import UserError
+
+
+class PosCreateNullReceiptWizard(models.TransientModel):
+    _name = 'pos.nullreceipt.wizard'
+    _description = 'Creates a null receipt in the RSKV register'
+
+    def _default_config(self):
+        active_id = self.env.context.get('active_id')
+        if active_id:
+            return self.env['pos.order'].browse(active_id).session_id.config_id
+        return False
+
+    config_id = fields.Many2one('pos.config', string='Point of Sale Configuration', required=True, default=_default_config)
+    description = fields.Text("Description", help="Provide a reason for the null receipt.")
+
+    def create_receipt(self):
+        if not self.config_id.pos_use_registrierkasse:
+            raise UserError(_("This function is only available for PoS with enabled RKSV."))
+
+        order = self.config_id.create_null_receipt(self.description)
+        if order:
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'pos.order',
+                'views': [[False, "form"]],
+                'res_id': order.id,
+            }
+        else:
+            raise UserError(_("Null receipt could not be created."))
