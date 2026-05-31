f5c9dba7fc05985194ac47b559ac423243785f8f
Manuel Faux <manuel@conf.at>
2026-05-31 22:23:53 +0200

    Version 18.0.1.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Austrian Registered Cash Register",
-    'version': '18.0.1.0',
+    'version': '18.0.1.1',
     'category': 'Sales/Point Of Sale',
     'summary': "POS Extention to comply with Austrian Registrierkassenpflicht (RKSV)",
     'description': """
